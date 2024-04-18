7f1270087d78eddd3926d374337986559195fbbd
Joël Henri <ebosqua.edimo@gmail.com>
2024-04-18 21:08:50 +0200

    Authentification pseudo-terminée - Alice

--- Alice/Server/Utils/utils_functions.py
+++ Alice/Server/Utils/utils_functions.py
@@ -1,13 +1,23 @@
 import hashlib
+from flask import render_template
 
 def values(dictionnary: dict):
     return tuple(dictionnary.values())
 
-def success(message):
-    return {"Status": "success", "Message": message}
+def render_error_login(error):
+    return render_template("login.html.jinja", error = error)
 
-def failure(message):
-    return {"Status": "failure", "Message": message}
+def render_error_signup(error):
+    return render_template("signup.html.jinja", error = error)
+
+def render_success_signup(message):
+    return render_template("signup.html.jinja", message = message)
+
+# def success(message):
+#     return {"Status": "success", "Message": message}
+
+# def failure(message):
+#     return {"Status": "failure", "Message": message}
 
 def hashing(password):
     return hashlib.sha256(password.encode("utf-8")).hexdigest()
--- Alice/Server/server.py
+++ Alice/Server/server.py
@@ -3,7 +3,7 @@ from Classes.Database.database import Database
 from Classes.Session.session import Session
 from Classes.Request.Cookies.cookies import Cookies
 from Classes.ErrorInterface.Execution.execution import ExecutionError as Error
-from .Utils.utils_functions import values, success, failure, hashing
+from .Utils.utils_functions import values, hashing, render_error_login, render_error_signup, render_success_signup
 
 app = Flask("Alice")
 
@@ -28,70 +28,63 @@ def login():
     # print(cookie.get("bonsoir"))
     # return res
     if not session.get("id"):
-            label = "LOGIN"
-        # try:
-            db = Database()
-            cookies = Cookies(request)
-            if request.method == "GET":
-                arguments = {
-                    "mail": cookies.get("mail"),
-                    "theme": cookies.get("theme"),
-                    "dark_theme_url": url_for("static", filename="/CSS/dark.css"),
-                    "light_theme_url": url_for("static", filename="/CSS/light.css"),
-                    "theme_setter_url": url_for("static", filename="/JavaScript/setTheme.js")
-                }
-                return render_template("login.html.jinja", **arguments)
-            if request.method == "POST":
-                form_length = len(request.form)
-                try:
-                    if form_length == 3:
-                        mail, password, remember = values(request.form)
-                    elif form_length == 2:
-                        mail, password = values(request.form)
-                    else:
-                        abort(401)
-                except Exception:
-                    Error.resolve(error_identifier, label, Error.exception, f"{err}")    
-                # print(remember)
-                if mail and password:
-                    user = db.select("SELECT * FROM users WHERE mail = ?", (mail,))
+        label = "LOGIN"
+    # try:
+        db = Database()
+        cookies = Cookies(request)
+        if request.method == "GET":
+            arguments = {
+                "mail": cookies.get("mail"),
+                "theme": cookies.get("theme"),
+                "dark_theme_url": url_for("static", filename="/CSS/dark.css"),
+                "light_theme_url": url_for("static", filename="/CSS/light.css"),
+                "theme_setter_url": url_for("static", filename="/JavaScript/setTheme.js")
+            }
+            return render_template("login.html.jinja", **arguments)
+        if request.method == "POST":
+            form = request.form
+            form_length = len(form)
+            try:
+                if form_length == 3:
+                    mail, password, remember = values(form)
+                elif form_length == 2:
+                    mail, password = values(form)
+                else:
+                    abort(401)
+            except Exception:
+                Error.resolve(error_identifier, label, Error.exception, f"{err}")    
+            if mail and password:
+                user = db.select("SELECT * FROM users WHERE mail = ?", (mail,))
+                # try:
+                # except TypeError: 
+                #     Error.resolve(error_identifier, label, Error.type, "user")    
+                if len(user):
                     try:
                         id, firstname, lastname, mail, real_password = values(user[0])
-                    except TypeError: 
-                        Error.resolve(error_identifier, label, Error.type, "user")    
-                    if user[0]:
-                        try:
-                            # print(real_password)
-                            # print(hashlib.sha256(password.encode("utf-8")).hexdigest())
-                            if real_password == hashing(password):
-                                session.set({"id": id, "firstname": firstname, "lastname": lastname, "mail": mail})
-                                favorites = db.select_and_close("SELECT * FROM favorites WHERE id_user = ?", (id,))
-                                if len(favorites) != 0:
-                                    station_codes = []
-                                    for set in favorites:
-                                        station_codes.append(set["station_code"])
-                                    session.set("favorites", station_codes)
-                                res = redirect(url_for("favorites"))
-                                if not cookies.get("theme"):
-                                    res = cookies.make("theme", "light", res)
-                                if form_length == 3:
-                                    if remember == "on":
-                                        res = cookies.make("mail", str(mail), res)
-                                if form_length == 2:
-                                    if cookies.get("mail"):
-                                        cookies.pop("mail", res)
-                                print(res.data)
-                                return res
-                            return failure("Le mot de passe renseigné ne correspond pas")
-                        except TypeError: 
-                            Error.resolve(error_identifier, label, Error.type, "user")    
-                        except Exception as err: 
-                            Error.resolve(error_identifier, label, Error.exception, f"{err}")    
-                    db.close()
-                    return failure("Aucun utilisateur ne possède l'adresse mail renseignée.")
-                return failure("L'un des champs renseignés est vide.")
-        # except Exception as err: 
-        #     Error.resolve(error_identifier, label, Error.exception, f"{err}")
+                        if real_password == hashing(password):
+                            session.set({"id": id, "firstname": firstname, "lastname": lastname, "mail": mail})
+                            favorites = db.select_and_close("SELECT * FROM favorites WHERE id_user = ?", (id,))
+                            if len(favorites):
+                                station_codes = []
+                                for set in favorites:
+                                    station_codes.append(set["station_code"])
+                                session.set("favorites", station_codes)
+                            res = redirect(url_for("favorites"))
+                            if not cookies.get("theme"):
+                                res = cookies.make("theme", "light", res)
+                            if form_length == 3:
+                                if remember == "on":
+                                    res = cookies.make("mail", str(mail), res)
+                            if form_length == 2:
+                                if cookies.get("mail"):
+                                    cookies.pop("mail", res)
+                            return res
+                        return render_error_login( "Le mot de passe renseigné ne correspond pas")
+                    except Exception as err: 
+                        Error.resolve(error_identifier, label, Error.exception, f"{err}")    
+                db.close()
+                return render_error_login("Aucun utilisateur ne possède l'adresse mail renseignée.")
+            return render_error_login("L'un des champs renseignés est vide.")
     return redirect(url_for("favorites"))
     
 @app.route("/signup", methods=["GET", "POST"])
@@ -103,17 +96,30 @@ def signup():
             if request.method == "GET":
                 return render_template("signup.html.jinja")
             if request.method == "POST":
-                firstname, lastname, mail, password = values(request.form)
-                user = db.select("SELECT * FROM users WHERE mail = ?", (mail,))
-                if user:
-                    db.close()
-                    return failure(["Un utilisateur possède déjà un espace rattaché à cette adresse mail"])
-                password = hashing(password)
-                db.mutate_and_close("INSERT INTO users (firstname, lastname, mail, password) VALUES (?,?,?,?)", (firstname, lastname, mail, password))
-                return success("La création de votre compte utilisateur a réussi")
+                form = request.form
+                form_length = len(form)
+                try:
+                    if form_length == 4:
+                        firstname, lastname, mail, password = values(form)
+                    else:
+                        abort(401)
+                except Exception:
+                    Error.resolve(error_identifier, label, Error.exception, f"{err}")
+                if firstname and mail and password:
+                    user = db.select("SELECT * FROM users WHERE mail = ?", (mail,))
+                    if len(user):
+                        db.close()
+                        return render_error_signup("Un utilisateur possède déjà un espace rattaché à cette adresse mail")
+                    password = hashing(password)
+                    if not lastname:
+                        db.mutate_and_close("INSERT INTO users (firstname, lastname, mail, password) VALUES (?, NULL,?,?)", (firstname, mail, password))
+                    else:
+                        db.mutate_and_close("INSERT INTO users (firstname, lastname, mail, password) VALUES (?,?,?,?)", (firstname, lastname, mail, password))
+                    return render_success_signup("La création de votre compte utilisateur a réussi")
+                return render_error_signup("L'un des champs obligatoires est vide")
         except Exception as err: 
             Error.resolve(error_identifier, label, Error.exception, f"{err}")
-    else: return redirect(url_for("favorites"))
+    return redirect(url_for("favorites"))
     
 @app.route("/logout")
 def logout():
